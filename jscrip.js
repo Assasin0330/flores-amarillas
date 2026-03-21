c38fd06a41bbec721ae5098ac6b86ec5590b305e
Assasin0330 <hugoyahirvm@gmail.com>
2026-03-21 00:11:15 -0600

    Add files via upload

---
+++ jscrip.js
@@ -0,0 +1,50 @@
+// Espera a que cargue la página
+document.addEventListener("DOMContentLoaded", () => {
+
+    const mensaje = document.querySelector(".mensaje");
+
+    // Aparece el mensaje después de 1 segundo
+    setTimeout(() => {
+        mensaje.style.animation = "aparecer 2s ease forwards";
+    }, 1000);
+
+
+    // Efecto cuando haces click en el ramo
+    const ramo = document.querySelector(".ramo");
+
+    ramo.addEventListener("click", () => {
+        mensaje.textContent = "Las amo mucho💛🌼";
+        mensaje.style.animation = "aparecer 1s ease forwards";
+    });
+
+});
+
+// Generar flores de fondo
+const fondo = document.querySelector(".fondo");
+
+for(let i = 0; i < 30; i++){
+    let flor = document.createElement("div");
+    flor.classList.add("flor-fondo");
+
+    flor.style.left = Math.random() * 100 + "vw";
+    flor.style.animationDuration = (3 + Math.random() * 5) + "s";
+    flor.style.opacity = Math.random();
+
+    fondo.appendChild(flor);
+}
+
+const btn = document.getElementById("btnMusica");
+const musica = document.getElementById("musica");
+
+let reproduciendo = false;
+
+btn.addEventListener("click", () => {
+    if(!reproduciendo){
+        musica.play();
+        btn.textContent = "⏸️ Pausar música";
+    } else {
+        musica.pause();
+        btn.textContent = "🎵 Reproducir música";
+    }
+    reproduciendo = !reproduciendo;
+});
